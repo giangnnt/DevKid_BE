f60121e4f3f372badcfa606dbb8a70ccba5c1ebd
Nguyen Giang <giangnnt260703@gmail.com>
2025-02-21 16:40:27 +0700

    hotfix user get

--- src/Application/Dto/UserDto.cs
+++ src/Application/Dto/UserDto.cs
@@ -9,7 +9,6 @@ namespace DevKid.src.Application.Dto
         public int RoleId { get; set; }
         public string Name { get; set; } = null!;
         public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
     }
     public class UserCreateDto
     {
